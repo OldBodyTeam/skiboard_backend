c48523bb49403450b62e4a8ce1783f82feacbe9c
zhangzhongyou <zhangzhongyou@bytedance.com>
2024-06-12 20:29:18 +0800

    feat: 新增页面UX

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,10 +3,10 @@ module.exports = {
     {
       name: 'lumii-backend',
       script: 'dist/main.js', // 你的应用程序的入口文件
-      instances: '2', // 根据你的需求设置实例数量
+      instances: '1', // 根据你的需求设置实例数量
       autorestart: true, // 如果应用程序崩溃，自动重启
       watch: false, // 是否监视文件变化并自动重启
-      max_memory_restart: '1G', // 如果内存占用超过1G，自动重启
+      max_memory_restart: '10G', // 如果内存占用超过1G，自动重启
       env: {
         NODE_ENV: 'production', // 设置环境变量
       },
